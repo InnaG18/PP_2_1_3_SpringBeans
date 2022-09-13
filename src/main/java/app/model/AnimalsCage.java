8375dd3d17e7d606d1e613a4991e4e87f60afe98
InnaG18 <endddddd1@gmail.com>
2022-09-13 15:44:57 +0300

    решение

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,38 @@
 package app.model;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Service
+@Getter
 @Component
 public class AnimalsCage {
 
     @Autowired
-    private Animal animal;
+    @Qualifier("cat")
+    private Animal cat;
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal dog;
+
+    @Autowired
+    private Timer timer;
+
+    public AnimalsCage(Animal cat, Animal dog, Timer timer) {
+        this.cat = cat;
+        this.dog = dog;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(cat.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
